befae3b2a249a71e636d9b7bc6f1a755158a6af4
Dania Asrar Butt <dania.asrar@devsinc.com>
2024-06-20 14:25:33 -0400

    Create articles model and associate it with users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :authored_articles, class_name: "Article", foreign_key: "author_id", dependent: :destroy
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
